ce021327b7c67e1ff2670c7ae324f2abeffdec4b
Jan Müller <janmueller3698@gmail.com>
2019-09-12 21:14:18 +0200

    Fixed wrong exception being thrown on too high indices

--- src/main/kotlin/eu/yeger/prf/Projection.kt
+++ src/main/kotlin/eu/yeger/prf/Projection.kt
@@ -6,7 +6,7 @@ class Projection(private val index: Int) : Function() {
 
     init {
         if (index < 0) throw ProjectionException("Projection index can not be negative")
-        setRequiredArgumentCount(index)
+        setRequiredArgumentCount(index + 1)
     }
 
     override fun evaluate(vararg arguments: Long): Long = arguments[index]
