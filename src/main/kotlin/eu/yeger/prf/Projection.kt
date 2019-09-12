ece123fd89202939b666f49fd4fd534cb805c594
Jan Müller <janmueller3698@gmail.com>
2019-09-12 21:02:46 +0200

    Refactored Projection class

--- src/main/kotlin/eu/yeger/prf/Projection.kt
+++ src/main/kotlin/eu/yeger/prf/Projection.kt
@@ -5,14 +5,9 @@ import eu.yeger.prf.exception.ProjectionException
 class Projection(private val index: Int) : Function() {
 
     init {
-        if (index < 1) {
-            throw ProjectionException("Projection expects index of at least 1 but received 0")
-        }
-
+        if (index < 0) throw ProjectionException("Projection index can not be negative")
         setRequiredArgumentCount(index)
     }
 
-    override fun evaluate(vararg arguments: Long): Long {
-        return arguments[index - 1]
-    }
+    override fun evaluate(vararg arguments: Long): Long = arguments[index]
 }
