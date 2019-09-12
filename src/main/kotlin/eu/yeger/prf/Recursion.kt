c553838ea2ff1437237a2cd7488754584e5ef526
Jan Müller <janmueller3698@gmail.com>
2019-09-12 22:17:39 +0200

    Cleaned Recursion class

--- src/main/kotlin/eu/yeger/prf/Recursion.kt
+++ src/main/kotlin/eu/yeger/prf/Recursion.kt
@@ -4,8 +4,7 @@ import eu.yeger.prf.exception.FunctionException
 
 import kotlin.math.max
 
-class Recursion constructor(private val baseCaseFunction: Function, private val recursiveCaseFunction: Function) :
-    Function() {
+class Recursion(private val baseCaseFunction: Function, private val recursiveCaseFunction: Function) : Function() {
 
     init {
         setArity(
@@ -15,13 +14,9 @@ class Recursion constructor(private val baseCaseFunction: Function, private val 
             )
         )
     }
-
-    @Throws(FunctionException::class)
-    override fun evaluate(vararg arguments: Long): Long {
-        return if (arguments[0] == 0L) { //base case
-            baseCaseFunction.apply(*arguments)
-        } else { //recursive case
-            recursiveCaseFunction.apply(*arguments)
-        }
+    
+    override fun evaluate(vararg arguments: Long): Long = when(arguments[0]) {
+        0L -> baseCaseFunction.apply(*arguments)
+        else -> recursiveCaseFunction.apply(*arguments)
     }
 }
