1d1a206ec51e09aaddbd9f27ab9743b8d5845fef
Jan Müller <janmueller3698@gmail.com>
2019-09-12 21:23:34 +0200

    Refactor of Function class
    Removed unnecessary requireNonNull and changed to expression bodies where possible

--- src/main/kotlin/eu/yeger/prf/Function.kt
+++ src/main/kotlin/eu/yeger/prf/Function.kt
@@ -4,8 +4,6 @@ import eu.yeger.prf.exception.ArityException
 import eu.yeger.prf.exception.CompositionException
 import eu.yeger.prf.exception.FunctionException
 
-import java.util.Objects
-
 abstract class Function {
 
     var requiredArgumentCount = 0
@@ -20,33 +18,26 @@ abstract class Function {
     }
 
     @Throws(FunctionException::class)
-    internal fun apply(vararg arguments: Long): Long {
-        if (requiredArgumentCount > arguments.size) {
+    internal fun apply(vararg arguments: Long): Long =
+        if (requiredArgumentCount > arguments.size)
             throw ArityException(requiredArgumentCount, arguments.size)
-        }
-
-        return evaluate(*arguments)
-    }
+        else
+            evaluate(*arguments)
 
     @Throws(FunctionException::class)
-    fun compose(vararg functions: Function): Function {
-        if (requiredArgumentCount > functions.size) {
+    fun compose(vararg functions: Function): Function =
+        if (requiredArgumentCount > functions.size)
             throw CompositionException(requiredArgumentCount, functions.size)
-        }
-
-        return Composition(this, *functions)
-    }
+        else
+            Composition(this, *functions)
 
     @Throws(FunctionException::class)
-    fun andThen(function: Function): Function {
-        Objects.requireNonNull(function)
-
-        if (function.requiredArgumentCount > 1) {
+    fun andThen(function: Function): Function =
+        if (function.requiredArgumentCount > 1)
             throw CompositionException("Function requires more than 1 argument")
-        }
+        else
+            function.compose(this)
 
-        return function.compose(this)
-    }
 
     @Throws(FunctionException::class)
     protected abstract fun evaluate(vararg arguments: Long): Long
