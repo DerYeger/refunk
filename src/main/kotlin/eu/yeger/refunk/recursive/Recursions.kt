f66d2f29c12d5265a69d5d5f681a067134edddaa
Jan Müller <janmueller3698@gmail.com>
2019-09-19 15:51:37 +0200

    Refactor

--- src/main/kotlin/eu/yeger/refunk/recursive/Recursions.kt
+++ src/main/kotlin/eu/yeger/refunk/recursive/Recursions.kt
@@ -4,10 +4,10 @@ import eu.yeger.refunk.base.Function
 
 class RecursionBuilder(val recursiveCase: Function)
 
-inline infix fun RecursionBuilder.withBaseCase(provider: () -> Function) = Recursion(provider.invoke(), this.recursiveCase)
-
-infix fun RecursionBuilder.withBaseCase(base: Function) = Recursion(base, this.recursiveCase)
-
 inline fun recursive(provider: () -> Function) = RecursionBuilder(provider.invoke())
 
 fun recursive(function: Function) = RecursionBuilder(function)
+
+inline infix fun RecursionBuilder.withBaseCase(provider: () -> Function) = Recursion(provider.invoke(), this.recursiveCase)
+
+infix fun RecursionBuilder.withBaseCase(base: Function) = Recursion(base, this.recursiveCase)
