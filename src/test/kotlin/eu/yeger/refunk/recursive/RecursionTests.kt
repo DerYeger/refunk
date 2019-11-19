5636da43927c39a0e76115ad3f96bcfd08ba87c4
Jan Müller <janmueller3698@gmail.com>
2019-11-20 00:29:27 +0100

    Expanded test coverage to recently added macros

--- src/test/kotlin/eu/yeger/refunk/recursive/RecursionTests.kt
+++ src/test/kotlin/eu/yeger/refunk/recursive/RecursionTests.kt
@@ -10,6 +10,20 @@ import org.junit.jupiter.api.Test
 class RecursionTests {
 
     @Test
+    fun testRecursionMacros() {
+        assertEquals(0, recursionResult(0, 1, 2, 3, 4))
+        assertEquals(1, recursionParameter(0, 1, 2, 3, 4))
+
+        assertEquals(2, firstRecursionArgument(0, 1, 2, 3, 4))
+        assertEquals(3, secondRecursionArgument(0, 1, 2, 3, 4))
+        assertEquals(4, thirdRecursionArgument(0, 1, 2, 3, 4))
+
+        assertEquals(2, firstBaseCaseArgument(2, 3, 4))
+        assertEquals(3, secondBaseCaseArgument(2, 3, 4))
+        assertEquals(4, thirdBaseCaseArgument(2, 3, 4))
+    }
+
+    @Test
     fun testRecursion() {
         assertEquals(10, Recursion(constant(10), constant(42))(0))
         assertEquals(42, Recursion(constant(10), constant(42))(1))
